0253e90f3514f1c91cbfa049ce365be7f5fbc89b
Nauval Rizky <enuma.alrizky@gmail.com>
2021-11-16 05:01:37 +0100

    techpack: display: temporary disable DC dimming when in fod_hbm mode
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -239,11 +239,9 @@ struct dsi_panel {
 	
 	int hbm_mode;
 
-#ifdef CONFIG_OSSFOD
 	struct brightness_alpha_pair *fod_dim_lut;
 	u32 fod_dim_lut_count;
-#endif
-
+	bool resend_ea;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
